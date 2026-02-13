810ab7a9d7d8cad6d397105e371525990690959f
Rohan Chakraborti <rohan.chakraborti.fiem.cse24@teamfuture.in>
2026-02-14 01:26:58 +0530

    Added pixelated front to Sponsors/page.tsx

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -22,7 +22,7 @@ export default function Sponsors() {
       <Navbar />
       <div className="min-h-screen w-full flex flex-col items-center font-sans p-8 pt-[180px] md:pt-[190px] bg-cover bg-center bg-no-repeat bg-fixed" style={{ backgroundImage: 'url(/background.png)' }}>
         <main className="flex flex-col items-center w-full text-center space-y-8">
-          <h1 className="text-4xl md:text-6xl font-bold font-orbitron tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-red-300 to-orange-500 drop-shadow-lg">
+          <h1 className="text-3xl md:text-5xl font-bold font-press-start tracking-wider text-transparent bg-clip-text bg-gradient-to-r from-red-300 to-orange-500 drop-shadow-lg">
             Sponsors
           </h1>
 
